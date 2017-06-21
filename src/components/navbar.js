dc6153e5d5af84e8479c70f974d7cbb4cc24d3d1
mblair415 <mblair415@gmail.com>
2017-06-21 13:58:56 -0700

    nav outline.  search bar outline

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+  render () {
+    return (
+      <div>
+        <div class="layer" data-id="58f91fda09f7358c5f1d979b" style="width: 4.73934%; height: 7.12106%; left: 0%; top: 0%;">
+          <div class="layerWidth">70px</div>
+          <div class="layerHeight">70px</div>
+          <div class="cornerCircle topLeftCircle"></div>
+          <div class="edgeCircle horizontalEdgeCircle topCircle"></div>
+          <div class="cornerCircle topRightCircle"></div>
+          <div class="edgeCircle verticalEdgeCircle rightCircle"></div>
+          <div class="cornerCircle bottomRightCircle"></div>
+          <div class="edgeCircle horizontalEdgeCircle bottomCircle"></div>
+          <div class="cornerCircle bottomLeftCircle"></div>
+          <div class="edgeCircle verticalEdgeCircle leftCircle"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
